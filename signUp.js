1bb73acfbcd712d37c05185c84a1c825d01b6432
vnkylin266 <tuanta@ql6625.fr>
2022-04-21 17:03:27 +0700

    minor change

--- signUp.js
+++ signUp.js
@@ -3,10 +3,10 @@ function onSubmitSignUp(e){
     
     var inputUsername = document.forms["form"]["username"].value
     var inputPassword = document.forms["form"]["password"].value
-    var inputMaKH = document.forms["form"]["maKH"].value
+    var inputemailKH = document.forms["form"]["emailKH"].value
     var usernameValidate = document.getElementById("usernameValidate")
     var passwordValidate = document.getElementById("passwordValidate")
-    var maValidate = document.getElementById("maValidate")
+    var emailValidate = document.getElementById("emailValidate")
 
     if(inputUsername === ""){
         usernameValidate.innerHTML = "Không được để trống"
@@ -14,8 +14,8 @@ function onSubmitSignUp(e){
     if(inputPassword === ""){
         passwordValidate.innerHTML = "Không được để trống"
     }
-    if(inputMaKH === ""){
-       maValidate.innerHTML = "Không được để trống"
+    if(inputemailKH === ""){
+       emailValidate.innerHTML = "Không được để trống"
     }  
     if(inputPassword > 0 && inputPassword.length < 8){
         passwordValidate.innerHTML = "Mật khẩu tối thiểu 8 kí tự"
